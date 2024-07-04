f99807ed5f73bc1895d1e3e01a3859d645f38e76
Jose Alfredo Aguado <jose.aguado@misena.edu.co>
2024-07-04 11:15:49 -0500

    first commit

---
+++ assets/main.js
@@ -0,0 +1,153 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  // when we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== IMAGE GALLERY ===============*/
+function imgGallery() {
+  const mainImg = document.querySelector('.details__img'),
+    smallImg = document.querySelectorAll('.details__small-img');
+
+  smallImg.forEach((img) => {
+    img.addEventListener('click', function () {
+      mainImg.src = this.src;
+    });
+  });
+}
+
+imgGallery();
+
+/*=============== SWIPER CATEGORIES ===============*/
+var swiperCategories = new Swiper('.categories__container', {
+  spaceBetween: 24,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    350: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 24,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
+
+/*=============== SWIPER PRODUCTS ===============*/
+var swiperProducts = new Swiper('.new__container', {
+  spaceBetween: 24,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+});
+
+/*=============== PRODUCTS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+  });
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// get all sections that have an id defined
+const sections = document.querySelectorAll("section[id]");
+
+// add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  // get current scroll position
+  let scrollY = window.pageYOffset;
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 10,
+      sectionId = current.getAttribute("id");
+    /* - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector */
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+    }
+    else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+    }
+  })
+}
